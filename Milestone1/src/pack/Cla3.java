273ec6a44def1744f0e868662bc14cdf33960b4e
madhuri <madhuriravuri1300@gamil.com>
2020-06-12 07:29:43 +0530

    cla3

--- Milestone1/src/pack/Cla3.java
+++ Milestone1/src/pack/Cla3.java
@@ -1,5 +1,13 @@
 package pack;
 
 public class Cla3 {
+	public static void main (String[] args) {
+		int n1,n2,sum;
+		n1=Integer.parseInt(args[0]);
+		n2=Integer.parseInt(args[1]);
+		sum=n1+n2;
+		System.out.println("sum="+sum);
+		
+	}
 
 }
