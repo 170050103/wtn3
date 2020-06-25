5d9d3368186156307c7faf5acc00177b43486a45
madhuri <madhuriravuri1300@gamil.com>
2020-06-25 20:35:28 +0530

    commit first

---
+++ Milestone1/src/arrays/a4.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class a4 {
+	public static void main(String args[])
+	{
+		int num[]= {119,120,98,115};
+		String str=null;
+		for(int i:num)
+		{
+			str=Character.toString((char)i);
+			System.out.print( str);
+		}
+	}
+
+
+}
